6a9631ff4351be9c61790854cbe0446d397687e5
ernestchu <shchu@citi.sinica.edu.tw>
2023-08-23 11:38:05 +0800

    fix baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: '/medm/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   }
 })
